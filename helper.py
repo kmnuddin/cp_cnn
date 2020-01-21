c249e8cd9b99909b52f7130ea1e0fc4f5c5b2be3
kmnuddin <kmnuddin@memphis.edu>
2020-01-21 00:15:40 -0600

    train model using 150 epochs

--- helper.py
+++ helper.py
@@ -1,22 +1,25 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential, load_model, save_model
+from sklearn.metrics import classification_report
 import os
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import pandas as pd
 
 class Helper:
 
     def __init__(self):
         self.train_directory = 'data/train/'
-        self.validation_directory = 'data/train/'
+        self.validation_directory = 'data/validation/'
         self.test_directory = 'data/test/'
         self.model_directory = 'models/'
         self.results_directory = 'results/'
         self.logs_directory = 'logs/cnn-topomap-log-{}'
+        self.y_true = []
 
 
-    def construct_data_generator(self, batch_size=128, validation_split=0.2, target_size=(224,224), shuffle=True):
+    def construct_data_generator(self, batch_size=128, validation_split=None, target_size=(224,224), shuffle=True):
 
         datagen = ImageDataGenerator(rescale=1./255, validation_split=validation_split)
 
@@ -27,8 +30,26 @@ class Helper:
                                                 target_size=target_size, shuffle=shuffle, subset='validation')
 
         test_it = datagen.flow_from_directory(self.test_directory, batch_size=batch_size,
-                                          target_size=target_size, shuffle=shuffle)
+                                          target_size=target_size, shuffle=False)
 
+        self.y_true = test_it.classes
+
+        return train_it, validation_it, test_it
+
+    def construct_data_generator_w_validation(self, batch_size=128, target_size=(224,224), shuffle=True):
+
+        datagen = ImageDataGenerator(rescale=1./255)
+
+        train_it = datagen.flow_from_directory(self.train_directory, batch_size=batch_size,
+                                           target_size=target_size, shuffle=shuffle )
+
+        validation_it = datagen.flow_from_directory(self.validation_directory, batch_size=batch_size,
+                                                target_size=target_size, shuffle=shuffle)
+
+        test_it = datagen.flow_from_directory(self.test_directory, batch_size=batch_size,
+                                          target_size=target_size, shuffle=False)
+
+        self.y_true = test_it.classes
 
         return train_it, validation_it, test_it
 
@@ -38,6 +59,9 @@ class Helper:
             path = self.train_directory
         if example_type is 'test':
             path = self.test_directory
+        if example_type is 'validation':
+            path = self.validation_directory
+
 
         img_cls_0 = os.listdir(path + str(classes[0]))
         img_cls_1 = os.listdir(path + str(classes[1]))
@@ -63,14 +87,22 @@ class Helper:
          model = load_model(path)
          return model
 
-    def save(self, model, evaluator, name):
+    def save(self, model, history, evaluator, y_prob, name):
          path_model = self.model_directory + name + '.h5'
          path_results = self.results_directory + name + '.txt'
 
          save_model(model, path_model)
          print('model saved, path: {}'.format(path_model))
 
+         y_pred = np.argmax(y_prob, axis=1)
+
+         report = pd.DataFrame(classification_report(self.y_true, y_pred, output_dict=True)).transpose()
+
          with open(path_results, 'a') as file:
              model.summary(print_fn = lambda x: file.write(x + '\n'))
              result = '{}:{}\n{}:{}'.format(model.metrics_names[0], evaluator[0], model.metrics_names[1], evaluator[1])
              file.write(result)
+             file.write('\n')
+             file.write('trained using {} epochs'.format(len(history.epoch)))
+             file.write('\n')
+             file.write(report.to_string())
